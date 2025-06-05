833ea9067dd068282fd097fd58e48d24670220fd
allkilk <giafdesign@gmail.com>
2025-06-05 17:32:04 +0200

    Added Text component import in Login and Register pages

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Box, Heading, Input, Button, FormControl, FormLabel, VStack, Alert, AlertIcon, Link as ChakraLink } from '@chakra-ui/react';
+import { Box, Heading, Input, Button, FormControl, FormLabel, VStack, Alert, AlertIcon, Link as ChakraLink, Text } from '@chakra-ui/react';
 import { Link as RouterLink, useNavigate } from 'react-router-dom';
 import { useAuth } from '../contexts/AuthContext';
 
